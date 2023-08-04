4596827e6d1e68214a5899252fa96c83429515c9
espinojosege <EspinojoseG@gmail.com>
2023-08-03 21:22:49 -0700

    changed the index.js to render week 15 first

--- src/index.js
+++ src/index.js
@@ -32,6 +32,14 @@ navbar.render(
   />
 );
 
+root.render(
+  <Weather />
+
+  /*<React.StrictMode>
+      <App />
+    </React.StrictMode>*/
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
